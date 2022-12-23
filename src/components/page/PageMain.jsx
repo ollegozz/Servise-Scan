27b97cd740200d78fa5b8009a90fd1f4514846a4
Олег <OllegOzz@yandex.ru>
2022-12-23 14:55:05 +0300

    main-full

--- src/components/page/PageMain.jsx
+++ src/components/page/PageMain.jsx
@@ -6,6 +6,11 @@ import time from '../image/icon/time.svg'
 import loupe from '../image/icon/loupe.svg'
 import sheild from '../image/icon/sheild.svg'
 import arr from '../image/icon/arr.svg'
+import chek from '../image/icon/chek.svg'
+import lamp from '../image/icon/lamp.svg'
+import target from '../image/icon/target.svg'
+import nout from '../image/icon/nout.svg'
+
 
 export default function PageMain() {
     return (
@@ -76,22 +81,96 @@ export default function PageMain() {
                     <h1 className={css.title}>наши тарифы</h1>
                     <div className={css.tarifCards}>
                         <div className={css.tarifCard}>
-                            <div className={css.tarifHeader}>
+                            <div className={`${css.tarifHeader} ${css.beginer}`}>
                                 <div className={css.tarifTitle}>Beginner</div>
                                 <div className={css.tarifSubTitle}>Для небольшого исследования</div>
+                                <img className={css.tarifImg} src={lamp} alt="lamp" />
                             </div>
                             <div className={css.tarifPrice}>
-                                <div className={css.riceTitle}>799 ₽ <span>1 200 ₽</span></div>
+                                <div className={css.priceTitle}>799 ₽ <span>1 200 ₽</span></div>
                                 <div className={css.priceSubTitle}>или 150 ₽/мес. при рассрочке на 24 мес.</div>
                             </div>
-
                             <div className={css.tarifList}>
-                                <div className={css.listTitle}>В тариф входит:
-                                    <div className={css.listSubTitle}>или 150 ₽/мес. при рассрочке на 24 мес.</div>
-                                </div>
+                                <div className={css.listTitle}>В тариф входит:</div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Безлимитная история запросов</span>
+                                    </div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Безопасная сделка</span>
+                                    </div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Поддержка 24/7</span>
+                                    </div>
                                 
                             </div>
-                            
+                            <div className={css.btn}>
+                                <button className={`${css.button} ${css.btnAct}`}>Перейти в личный кабинет</button>
+                            </div>
+                        </div>
+
+                        <div className={css.tarifCard}>
+                            <div className={`${css.tarifHeader} ${css.pro}`}>
+                                <div className={css.tarifTitle}>Pro</div>
+                                <div className={css.tarifSubTitle}>Для HR и фрилансеров</div>
+                                <img className={`${css.tarifImg} ${css.targetImg}`} src={target} alt="target" />
+                            </div>
+                            <div className={css.tarifPrice}>
+                                <div className={css.priceTitle}>1 299 ₽ <span>2 600 ₽</span></div>
+                                <div className={css.priceSubTitle}>или 279 ₽/мес. при рассрочке на 24 мес.</div>
+                            </div>
+                            <div className={css.tarifList}>
+                                <div className={css.listTitle}>В тариф входит:</div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Все пункты тарифа Beginner</span>
+                                    </div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Экспорт истории</span>
+                                    </div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Рекомендации по приоритетам</span>
+                                    </div>
+                                
+                            </div>
+                            <div className={css.btn}>
+                                <button className={css.button}>Подробнее</button>
+                            </div>
+                        </div>
+
+                        <div className={css.tarifCard}>
+                            <div className={`${css.tarifHeader} ${css.business}`}>
+                                <div className={css.tarifTitle}>Business</div>
+                                <div className={css.tarifSubTitle}>Для корпоративных клиентов</div>
+                                <img className={css.tarifImg} src={nout} alt="nout" />
+                            </div>
+                            <div className={css.tarifPrice}>
+                                <div className={css.priceTitle}>2 379 ₽ <span>3 700 ₽</span></div>
+                                <div className={css.priceSubTitle}>или 279 ₽/мес. при рассрочке на 24 мес.</div>
+                            </div>
+                            <div className={css.tarifList}>
+                                <div className={css.listTitle}>В тариф входит:</div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Все пункты тарифа Pro</span>
+                                    </div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Безлимитное количество запросов</span>
+                                    </div>
+                                    <div className={css.listChek}>
+                                        <img src={chek} alt="" />
+                                        <span>Приоритетная поддержка</span>
+                                    </div>
+                                
+                            </div>
+                            <div className={css.btn}>
+                                <button className={css.button}>Подробнее</button>
+                            </div>
                         </div>
                     </div>
                 </div>

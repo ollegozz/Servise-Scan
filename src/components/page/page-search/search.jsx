d1508e533eda51518440b1a05b0051371fb88e25
Олег <OllegOzz@yandex.ru>
2023-01-09 16:38:59 +0300

    1

--- src/components/page/page-search/search.jsx
+++ src/components/page/page-search/search.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './search.module.css'
 
 export default function Search() {
+
+const [inn, setInn] = useState()
+  const [state, setState] = useState()
+
+
+  const handleChange = (e) => {
+    setState({ value: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Отправленное имя: ' + state.value);
+    
+  }
+
+
   return (
     <main className={css.content}>
-
-
+      <div className={css.form}>
+        <form onSubmit={handleSubmit}>
+          <label>
+            ИНН компании*
+            <input type="text"  onChange={handleChange} />
+          </label>
+          <input type="submit" value="Отправить" />
+        </form>
+      </div>
     </main>
   )
 }

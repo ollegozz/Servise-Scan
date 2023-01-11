2f265862c3613b8a6284696ef635f2fda94772ee
Олег <OllegOzz@yandex.ru>
2023-01-11 20:51:31 +0300

    search-input

---
+++ src/components/UI/input/input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from './input.module.css'
+
+export default function Input(props) {
+
+    return (
+        <input className={css.input} {...props}>
+
+        </input>
+    )
+}
--- src/components/page/page-search/search.jsx
+++ src/components/page/page-search/search.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from 'react'
+import Input from '../../UI/input/input'
 import css from './search.module.css'
 
 export default function Search() {
 
 const [inn, setInn] = useState()
-  const [state, setState] = useState()
+const [amtDoc, setAmtDoc] = useState()
 
+console.log(inn);
+  console.log(amtDoc);
 
-  const handleChange = (e) => {
-    setState({ value: e.target.value });
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Отправленное имя: ' + state.value);
-    
-  }
 
 
   return (
     <main className={css.content}>
       <div className={css.form}>
-        <form onSubmit={handleSubmit}>
-          <label>
-            ИНН компании*
-            <input type="text"  onChange={handleChange} />
-          </label>
-          <input type="submit" value="Отправить" />
+        <form>
+          <div className={css.formTitle}>ИНН компании*</div>
+          <Input 
+            placeholder='10 цифр'
+            onChange={e => setInn(e.target.value)}
+          />
+
+          <div className={css.formTitle}>Количество документов в выдаче</div>
+          <Input
+            placeholder='От 1 до 1000'
+            onChange={e => setAmtDoc(e.target.value)}
+          />
         </form>
       </div>
     </main>

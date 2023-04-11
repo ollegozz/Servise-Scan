1f9ffd336ba93c816b68acbc4b3beb2827ff73cd
Олег <OllegOzz@yandex.ru>
2023-04-11 15:31:10 +0300

    authRequest

--- src/components/popup/auth.jsx
+++ src/components/popup/auth.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import css from './auth.module.css'
 import google from '../image/icon/google.png'
 import facebook from '../image/icon/facebook.png'
@@ -8,18 +8,39 @@ import yandex from '../image/icon/yandex.png'
 
 export default function Auth() {
 
-    const [login, setLogin] = useState('sf_student6')
-    const [pass, setPass] = useState('73r%Lus9Wn')     
+    const [login, setLogin] = useState('sf_student9')
+    const [pass, setPass] = useState('k%3E%nJF9y')
+    // const [status, setStatus] = useState([])
 
-    function getAuth() {
-        fetch(`https://gateway.scan-interfax.ru/api/v1/account/login/${login}`)
-        .then(data => {
-            console.log(data);
+    async function getAuth() {
+        const url = `https://gateway.scan-interfax.ru/api/v1/account/login/`;
+
+        return await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ login: login, password: pass }),
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
         })
+            .then(data => {
+                console.log('data', data);
+            })
+            .catch(error => {
+                console.log('error', error);
+    })
+
+        // }).then((res) => setStatus(res.json())) 
+                    
     }
 
+    // console.log(status);
+
+
+
+
     return (
-        
+
         <div className={css.body}>
             <div className={css.btnTop}>
                 <button className={css.btnAuth}>Войти</button>
@@ -28,7 +49,7 @@ export default function Auth() {
 
             <div className={css.input}>
                 <p>Логин или номер телефона:</p>
-                <input type="text" 
+                <input type="text"
                     onChange={e => setLogin(e.target.value)}
                     value={login}
                 />
@@ -36,18 +57,18 @@ export default function Auth() {
 
             <div className={css.input}>
                 <p>Пароль:</p>
-                <input type="password" 
-                onChange={e => setPass(e.target.value)}
-                value={pass}
+                <input type="password"
+                    onChange={e => setPass(e.target.value)}
+                    value={pass}
                 />
             </div>
 
             <div className={css.btnSignIn}>
                 <button className={css.button}
-                onClick={getAuth}
+                    onClick={getAuth}
                 >
                     Войти
-                    </button>
+                </button>
                 <a href="/#">Восстановить пароль</a>
             </div>
 

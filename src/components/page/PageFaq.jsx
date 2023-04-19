0bb28b5254d1d1aa0d7386b8484b091357d98f38
Олег <OllegOzz@yandex.ru>
2023-04-19 18:12:47 +0300

    PageFaq_PageTarifs

--- src/components/page/PageFaq.jsx
+++ src/components/page/PageFaq.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function PageFaq() {
     return (
-        <div>
+        <div style={{ height: '80vh' }}>
             <h2>FAQ</h2>
+            <p style={{ fontSize: '30px', paddingTop: '50px' }}>Страница в разработке</p>
         </div>
     )
 }
--- src/components/page/PageTarifs.jsx
+++ src/components/page/PageTarifs.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function PageTarifs() {
     return (
-        <div>
-            <h3>PageTarifs</h3>
+        <div style={{height:'80vh'}}>
+            <h2>Тарифы</h2>
+            <p style={{fontSize:'30px', paddingTop:'50px'}}>Страница в разработке</p>
         </div>
     )
 }
